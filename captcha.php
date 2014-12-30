509c152bc145066887d52d8be355bb979c7cc2d7
shyppmoon <binghe235@126.com>
2014-12-30 22:54:38 +0800

    换成新的验证码类，评论列表修改，去除why choose me，调整样式

--- captcha.php
+++ captcha.php
@@ -19,12 +19,15 @@ define('INIT_NO_SMARTY', true);
 require(dirname(__FILE__) . '/includes/init.php');
 require(ROOT_PATH . 'includes/cls_captcha.php');
 
-$img = new captcha(ROOT_PATH . 'data/captcha/', $_CFG['captcha_width'], $_CFG['captcha_height']);
+$_vc = new captcha();      //实例化一个对象
+
 @ob_end_clean(); //清除之前出现的多余输入
 if (isset($_REQUEST['is_login']))
 {
     $img->session_word = 'captcha_login';
 }
-$img->generate_image();
+
+$_vc->doimg();
+$_SESSION['captcha_word'] = $_vc->getCode();
 
 ?>
--- includes/cls_captcha.php
+++ includes/cls_captcha.php
@@ -17,268 +17,104 @@ if (!defined('IN_ECS'))
 {
     die('Hacking attempt');
 }
+//验证码类
+class captcha {
+	private $charset = 'abcdefghkmnprstuvwxyzABCDEFGHKMNPRSTUVWXYZ23456789'; //随机因子
+	private $code;       //验证码
+	private $codelen = 4;     //验证码长度
+	private $width = 130;     //宽度
+	private $height = 22;     //高度
+	private $img;        //图形资源句柄
+	private $font;        //指定的字体
+	private $fontsize = 12;    //指定字体大小
+	private $fontcolor;      //指定字体颜色
+	
+	/*------------------------------------------------------ */
+	//-- 存在session中的名称
+	/*------------------------------------------------------ */
+	var $session_word = 'captcha_word';
 
-class captcha
-{
-    /**
-     * 背景图片所在目录
-     *
-     * @var string  $folder
-     */
-    var $folder     = 'data/captcha';
+	//构造方法初始化
+	public function __construct() {
+		$this->font = ROOT_PATH.'/font/elephant.ttf';
+	}
 
-    /**
-     * 图片的文件类型
-     *
-     * @var string  $img_type
-     */
-    var $img_type   = 'png';
+	//生成随机码
+	private function createCode() {
+		$_len = strlen($this->charset)-1;
+		for ($i=0;$i<$this->codelen;$i++) {
+			$this->code .= $this->charset[mt_rand(0,$_len)];
+		}
+	}
+	
+	/**
+	 * 检查给出的验证码是否和session中的一致
+	 *
+	 * @access  public
+	 * @param   string  $word   验证码
+	 * @return  bool
+	 */
+	function check_word($word)
+	{
+		if(isset($_SESSION[$this->session_word])){
+			if($_SESSION[$this->session_word] == strtolower($word)){
+				return  true;
+			}else{
+				return  false;
+			}
+		}
+	}
+	
+	
+	//生成背景
+	private function createBg() {
+		$this->img = imagecreatetruecolor($this->width, $this->height);
+		$color = imagecolorallocate($this->img, mt_rand(157,255), mt_rand(157,255), mt_rand(157,255));
+		imagefilledrectangle($this->img,0,$this->height,$this->width,0,$color);
+	}
 
-    /*------------------------------------------------------ */
-    //-- 存在session中的名称
-    /*------------------------------------------------------ */
-    var $session_word = 'captcha_word';
+	//生成文字
+	private function createFont() {
+		$_x = $this->width / $this->codelen;
+		for ($i=0;$i<$this->codelen;$i++) {
+			$this->fontcolor = imagecolorallocate($this->img,mt_rand(0,156),mt_rand(0,156),mt_rand(0,156));
+			imagettftext($this->img,$this->fontsize,mt_rand(-30,30),$_x*$i+mt_rand(1,5),$this->height /1.2,$this->fontcolor,$this->font,$this->code[$i]);
+		}
+	}
 
-    /**
-     * 背景图片以及背景颜色
-     *
-     * 0 => 背景图片的文件名
-     * 1 => Red, 2 => Green, 3 => Blue
-     * @var array   $themes
-     */
-    var $themes_jpg = array(
-        1 => array('captcha_bg1.jpg', 255, 255, 255),
-        2 => array('captcha_bg2.jpg', 0, 0, 0),
-        3 => array('captcha_bg3.jpg', 0, 0, 0),
-        4 => array('captcha_bg4.jpg', 255, 255, 255),
-        5 => array('captcha_bg5.jpg', 255, 255, 255),
-    );
+	//生成线条、雪花
+	private function createLine() {
+		for ($i=0;$i<6;$i++) {
+			$color = imagecolorallocate($this->img,mt_rand(0,156),mt_rand(0,156),mt_rand(0,156));
+			imageline($this->img,mt_rand(0,$this->width),mt_rand(0,$this->height),mt_rand(0,$this->width),mt_rand(0,$this->height),$color);
+		}
+		for ($i=0;$i<100;$i++) {
+			$color = imagecolorallocate($this->img,mt_rand(200,255),mt_rand(200,255),mt_rand(200,255));
+			imagestring($this->img,mt_rand(1,5),mt_rand(0,$this->width),mt_rand(0,$this->height),'*',$color);
+		}
+	}
 
-    var $themes_gif = array(
-        1 => array('captcha_bg1.gif', 255, 255, 255),
-        2 => array('captcha_bg2.gif', 0, 0, 0),
-        3 => array('captcha_bg3.gif', 0, 0, 0),
-        4 => array('captcha_bg4.gif', 255, 255, 255),
-        5 => array('captcha_bg5.gif', 255, 255, 255),
-    );
+	//输出
+	private function outPut() {
+		header('Content-type:image/png');
+		imagepng($this->img);
+		imagedestroy($this->img);
+	}
 
-    /**
-     * 图片的宽度
-     *
-     * @var integer $width
-     */
-    var $width      = 130;
+	//对外生成
+	public function doimg() {
+		$this->createBg();
+		$this->createCode();
+		$this->createLine();
+		$this->createFont();
+		$this->outPut();
+	}
 
-    /**
-     * 图片的高度
-     *
-     * @var integer $height
-     */
-    var $height     = 20;
+	//获取验证码
+	public function getCode() {
+		return strtolower($this->code);
+	}
 
-    /**
-     * 构造函数
-     *
-     * @access  public
-     * @param   string  $folder     背景图片所在目录
-     * @param   integer $width      图片宽度
-     * @param   integer $height     图片高度
-     * @return  bool
-     */
-    function captcha($folder = '', $width = 145, $height = 20)
-    {
-        if (!empty($folder))
-        {
-            $this->folder = $folder;
-        }
-
-        $this->width    = $width;
-        $this->height   = $height;
-
-        /* 检查是否支持 GD */
-        if (PHP_VERSION >= '4.3')
-        {
-
-            return (function_exists('imagecreatetruecolor') || function_exists('imagecreate'));
-        }
-        else
-        {
-
-            return (((imagetypes() & IMG_GIF) > 0) || ((imagetypes() & IMG_JPG)) > 0 );
-        }
-    }
-
-    /**
-     * 构造函数
-     *
-     * @access  public
-     * @param
-     *
-     * @return void
-     */
-    function __construct($folder = '', $width = 145, $height = 20)
-    {
-        $this->captcha($folder, $width, $height);
-    }
-
-
-    /**
-     * 检查给出的验证码是否和session中的一致
-     *
-     * @access  public
-     * @param   string  $word   验证码
-     * @return  bool
-     */
-    function check_word($word)
-    {
-        $recorded = isset($_SESSION[$this->session_word]) ? base64_decode($_SESSION[$this->session_word]) : '';
-        $given    = $this->encrypts_word(strtoupper($word));
-
-        return (preg_match("/$given/", $recorded));
-    }
-
-    /**
-     * 生成图片并输出到浏览器
-     *
-     * @access  public
-     * @param   string  $word   验证码
-     * @return  mix
-     */
-    function generate_image($word = false)
-    {
-        if (!$word)
-        {
-            $word = $this->generate_word();
-        }
-
-        /* 记录验证码到session */
-        $this->record_word($word);
-
-        /* 验证码长度 */
-        $letters = strlen($word);
-
-        /* 选择一个随机的方案 */
-        mt_srand((double) microtime() * 1000000);
-
-        if (function_exists('imagecreatefromjpeg') && ((imagetypes() & IMG_JPG) > 0))
-        {
-            $theme  = $this->themes_jpg[mt_rand(1, count($this->themes_jpg))];
-        }
-        else
-        {
-            $theme  = $this->themes_gif[mt_rand(1, count($this->themes_gif))];
-        }
-
-        if (!file_exists($this->folder . $theme[0]))
-        {
-            return false;
-        }
-        else
-        {
-            $img_bg    = (function_exists('imagecreatefromjpeg') && ((imagetypes() & IMG_JPG) > 0)) ?
-                            imagecreatefromjpeg($this->folder . $theme[0]) : imagecreatefromgif($this->folder . $theme[0]);
-            $bg_width  = imagesx($img_bg);
-            $bg_height = imagesy($img_bg);
-
-            $img_org   = ((function_exists('imagecreatetruecolor')) && PHP_VERSION >= '4.3') ?
-                          imagecreatetruecolor($this->width, $this->height) : imagecreate($this->width, $this->height);
-
-            /* 将背景图象复制原始图象并调整大小 */
-            if (function_exists('imagecopyresampled') && PHP_VERSION >= '4.3') // GD 2.x
-            {
-                imagecopyresampled($img_org, $img_bg, 0, 0, 0, 0, $this->width, $this->height, $bg_width, $bg_height);
-            }
-            else // GD 1.x
-            {
-                imagecopyresized($img_org, $img_bg, 0, 0, 0, 0, $this->width, $this->height, $bg_width, $bg_height);
-            }
-            imagedestroy($img_bg);
-
-            $clr = imagecolorallocate($img_org, $theme[1], $theme[2], $theme[3]);
-
-            /* 绘制边框 */
-            //imagerectangle($img_org, 0, 0, $this->width - 1, $this->height - 1, $clr);
-
-            /* 获得验证码的高度和宽度 */
-            $x = ($this->width - (imagefontwidth(5) * $letters)) / 2;
-            $y = ($this->height - imagefontheight(5)) / 2;
-            imagestring($img_org, 5, $x, $y, $word, $clr);
-
-            header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
-
-            // HTTP/1.1
-            header('Cache-Control: private, no-store, no-cache, must-revalidate');
-            header('Cache-Control: post-check=0, pre-check=0, max-age=0', false);
-
-            // HTTP/1.0
-            header('Pragma: no-cache');
-            if ($this->img_type == 'jpeg' && function_exists('imagecreatefromjpeg'))
-            {
-                header('Content-type: image/jpeg');
-                imageinterlace($img_org, 1);
-                imagejpeg($img_org, false, 95);
-            }
-            else
-            {
-                header('Content-type: image/png');
-                imagepng($img_org);
-            }
-
-            imagedestroy($img_org);
-
-            return true;
-        }
-    }
-
-    /*------------------------------------------------------ */
-    //-- PRIVATE METHODs
-    /*------------------------------------------------------ */
-
-    /**
-     * 对需要记录的串进行加密
-     *
-     * @access  private
-     * @param   string  $word   原始字符串
-     * @return  string
-     */
-    function encrypts_word($word)
-    {
-        return substr(md5($word), 1, 10);
-    }
-
-    /**
-     * 将验证码保存到session
-     *
-     * @access  private
-     * @param   string  $word   原始字符串
-     * @return  void
-     */
-    function record_word($word)
-    {
-        $_SESSION[$this->session_word] = base64_encode($this->encrypts_word($word));
-    }
-
-    /**
-     * 生成随机的验证码
-     *
-     * @access  private
-     * @param   integer $length     验证码长度
-     * @return  string
-     */
-    function generate_word($length = 4)
-    {
-        $chars = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
-
-        for ($i = 0, $count = strlen($chars); $i < $count; $i++)
-        {
-            $arr[$i] = $chars[$i];
-        }
-
-        mt_srand((double) microtime() * 1000000);
-        shuffle($arr);
-
-        return substr(implode('', $arr), 5, $length);
-    }
 }
 
 ?>
